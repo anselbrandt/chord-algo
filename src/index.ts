546af1fadecf086712bf49ca199a84f50cdd9df2
anselbrandt <ansel@anselbrandt.com>
2024-02-02 07:49:01 -0500

    Add chord generator

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log('hello')
+const shapes = [
+  [0, 4, 7],
+  [0, 3, 8],
+  [0, 5, 9],
+  [0, 3, 7],
+  [0, 4, 9],
+  [0, 5, 8],
+  [0, 4, 7, 11],
+  [0, 3, 7, 8],
+  [0, 4, 5, 9],
+  [0, 1, 5, 8],
+  [0, 3, 7, 10],
+  [0, 4, 7, 9],
+  [0, 3, 5, 8],
+  [0, 2, 5, 9],
+];
+
+// 88-key piano -> 21 - 108
+const chords = Array(88)
+  .fill(0)
+  .map((_, index) => index + 21)
+  .map((start) =>
+    shapes.map((shape) => shape.map((interval) => start + interval))
+  )
+  .flat()
+  .filter((chord) => !chord.some((note) => note > 108));
