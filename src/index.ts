426046f51a0ef2dedd4cd243a813fc56296ce674
anselbrandt <ansel@anselbrandt.com>
2024-02-03 22:12:01 -0500

    Use only seventh chords

--- src/index.ts
+++ src/index.ts
@@ -5,35 +5,31 @@ const playChord = async (port: any, chord: number[]) => {
   for (const note of chord) {
     await port.noteOn(0, note, 90);
   }
-  await port.wait(6000);
+  await port.wait(2000);
   for (const note of chord) {
     await port.noteOff(0, note);
   }
 };
 
-const getNextChord = (currentChord: number[]) => {
+const getNextChord = (currentChord: number[]): number[] => {
   const options = chordMap.get(currentChord);
   const chord = options[Math.floor(Math.random() * options.length)];
-  return chord;
+  if (chord !== currentChord) return chord;
+  return getNextChord(currentChord);
 };
 
 async function main() {
   const midi = await JZZ();
-  const ports = await midi.info().outputs;
-  const fluidPort = ports.filter((port: any) =>
-    port.id.includes("Fluidsynth")
-  )[0];
-  //   [
-  //     {
-  //       id: "Logic Pro Virtual In",
-  //       name: "Logic Pro Virtual In",
-  //       manufacturer: "Unknown",
-  //       version: "0.0",
-  //       engine: "node",
-  //     },
-  //   ]
-  //   const port = await midi.openMidiOut("Logic Pro Virtual In");
-  const port = await midi.openMidiOut(fluidPort);
+
+  // const ports = await midi.info().outputs;
+  // const fluidPort = ports.filter((port: any) =>
+  //   port.id.includes("FluidSynth")
+  // )[0];
+  // const logicPort = ports.filter((port: any) =>
+  //   port.id.includes("Logic Pro")
+  // )[0];
+
+  const port = await midi.openMidiOut();
   const seed = chords[Math.floor(Math.random() * chords.length)];
   let nextChord = null;
   try {
