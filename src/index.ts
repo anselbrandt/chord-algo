ff6f7aa72c3b5887fb2291ca5d4dddc672d30d0a
anselbrandt <ansel@anselbrandt.com>
2024-02-07 15:30:49 -0500

    Add bass

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,46 @@
 import JZZ from "jzz";
 import { chords, chordMap } from "./chords";
-import fs from "fs/promises";
 
-const playChord = async (ports: any, chord: number[]) => {
-  const [port1, port2] = ports;
+const MAX_DURATION = 8000;
+const LEAD_DELAY = 4000;
+const BASS_DELAY = 2000;
+
+let prevBass: number | null = null;
+
+const playChord = async (port: any, chord: number[]) => {
   for (const note of chord) {
-    await port1.noteOn(1, note, 60);
+    await port.noteOn(1, note, 60);
   }
+  await port.wait(MAX_DURATION);
+  for (const note of chord) {
+    await port.noteOff(1, note);
+  }
+};
+
+const playLead = async (port: any, chord: number[]) => {
   const lead = chord[Math.floor(Math.random() * chord.length)];
-  await port2.wait(1000);
-  await port2.noteOn(1, lead, 90);
-  await port2.wait(1000);
-  await port2.noteOff(1, lead);
+  await port.wait(LEAD_DELAY);
+  await port.noteOn(1, lead, 60);
+  await port.wait(MAX_DURATION - LEAD_DELAY);
+  await port.noteOff(1, lead);
+};
 
-  await port1.wait(4000);
-  for (const note of chord) {
-    await port1.noteOff(1, note);
-  }
+const playBass = async (port: any, chord: number[]) => {
+  let bass = chord[0] - 12;
+  if (bass === prevBass) bass = bass - 12;
+  prevBass = bass;
+  await port.wait(BASS_DELAY);
+  await port.noteOn(1, bass, 60);
+  await port.wait(MAX_DURATION - BASS_DELAY);
+  await port.noteOff(1, bass);
+};
+
+const playAll = async (ports: any, chord: number[]) => {
+  const [port1, port2, port3] = ports;
+  const playedChord = playChord(port1, chord);
+  const playedLead = playLead(port2, chord);
+  const playedBass = playBass(port3, chord);
+  await Promise.all([await playedChord, await playedLead, await playedBass]);
 };
 
 const getNextChord = (currentChord: number[]): number[] => {
@@ -27,11 +51,6 @@ const getNextChord = (currentChord: number[]): number[] => {
 };
 
 async function main() {
-  const arr = Array.from(chordMap);
-  const chordArr = arr.map((entry) => entry[0]);
-
-  console.log(chordMap.size);
-
   const midi = await JZZ();
 
   const ports = await midi.info().outputs;
@@ -44,8 +63,13 @@ async function main() {
     port.id.includes("IAC Driver Bus 2")
   )[0];
 
+  const IACport3 = ports.filter((port: any) =>
+    port.id.includes("IAC Driver Bus 3")
+  )[0];
+
   const port1 = await midi.openMidiOut(IACport1);
   const port2 = await midi.openMidiOut(IACport2);
+  const port3 = await midi.openMidiOut(IACport3);
 
   const seed = chords[Math.floor(Math.random() * chords.length)];
   let nextChord = null;
@@ -63,9 +87,7 @@ async function main() {
     while (true) {
       const currentChord: number[] = nextChord || seed;
       if (!prevChords.includes(currentChord)) {
-        const idx = chordArr.indexOf(currentChord);
-        console.log(idx);
-        await playChord([port1, port2], currentChord);
+        await playAll([port1, port2, port3], currentChord);
         addToPrevChords(currentChord);
       }
       nextChord = getNextChord(currentChord);
@@ -75,8 +97,10 @@ async function main() {
   } finally {
     await port1.allNotesOff(1);
     await port2.allNotesOff(1);
+    await port3.allNotesOff(1);
     await port1.close();
     await port2.close();
+    await port3.close();
   }
 }
 
--- src/stop.ts
+++ src/stop.ts
@@ -13,17 +13,24 @@ async function main() {
     port.id.includes("IAC Driver Bus 2")
   )[0];
 
+  const IACport3 = ports.filter((port: any) =>
+    port.id.includes("IAC Driver Bus 3")
+  )[0];
+
   const port1 = await midi.openMidiOut(IACport1);
   const port2 = await midi.openMidiOut(IACport2);
+  const port3 = await midi.openMidiOut(IACport3);
 
   try {
     await port1.allNotesOff(1);
     await port2.allNotesOff(1);
+    await port3.allNotesOff(1);
   } catch (error) {
     console.log(error);
   } finally {
     await port1.close();
     await port2.close();
+    await port3.close();
   }
 }
 
